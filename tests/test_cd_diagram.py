073e4a5115653e00b488f9aee0bf9cc9e56c91d3
albertoazzari <alberto.azzari@univr.it>
2025-07-18 11:50:09 +0300

    Refactor test for draw_cd_diagram to use synthetic data
    - Removed the commented-out code that read from a CSV file.
    - Introduced a random number generator to create synthetic models data.
    - Constructed a DataFrame with multiple models using normal distributions.
    - Updated the test to validate draw_cd_diagram with the new DataFrame.

--- tests/test_cd_diagram.py
+++ tests/test_cd_diagram.py
@@ -4,5 +4,22 @@ from cd_diagram import draw_cd_diagram
 
 # def test_draw_cd_diagram():
    
-samples = pd.read_csv("tests/test_data.csv", index_col=0, header=0)
-draw_cd_diagram(samples, labels=[x.upper() for x in samples.columns], title="TEST", out_file="tests/test.svg")
+# samples = pd.read_csv("tests/test_data.csv", index_col=0, header=0)
+# draw_cd_diagram(samples, labels=[x.upper() for x in samples.columns], title="TEST", out_file="tests/test.svg")
+rng = np.random.default_rng(1)
+
+models = {
+    'model1': rng.normal(loc=0.2, scale=0.1, size=30),
+    'model2': rng.normal(loc=0.2, scale=0.1, size=30),
+    'model3': rng.normal(loc=0.4, scale=0.1, size=30),
+    'model4': rng.normal(loc=0.5, scale=0.1, size=30),
+    'model5': rng.normal(loc=0.7, scale=0.1, size=30),
+    'model6': rng.normal(loc=0.7, scale=0.1, size=30),
+    'model7': rng.normal(loc=0.8, scale=0.1, size=30),
+    'model8': rng.normal(loc=0.9, scale=0.1, size=30),
+}
+
+models = pd.DataFrame(models)
+
+# it works with both a pandas Dataframe and a numpy array
+draw_cd_diagram(models, labels=models.columns.to_list(), out_file="tests/test.svg")
